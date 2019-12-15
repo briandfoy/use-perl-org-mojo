d0914982d2e78db13291f72140a843ce5543a089
brian d foy <brian.d.foy@gmail.com>
2019-12-14 23:24:20 -0500

    Add the basics for all the routes

--- lib/UsePerl.pm
+++ lib/UsePerl.pm
@@ -14,7 +14,16 @@ sub startup ($self) {
 	my $r = $self->routes;
 
 	# Normal route to controller
-	$r->get('/')->to('main#index');
+	$r->get('/')                             ->to('main#index');
+	$r->get('/about')                        ->to('main#about');
+
+	$r->get('/authors')                      ->to('main#authors');
+	$r->get('/journals')                     ->to('main#journals');
+	$r->get('/stories')                      ->to('main#stories');
+
+	$r->get('/~:author/')                    ->to('main#author');
+	$r->get('/~:author/journal/:journal')    ->to('main#journal');
+	$r->get('/article.pl')                   ->to('main#story');
 	}
 
 1;
--- lib/UsePerl/Controller/Main.pm
+++ lib/UsePerl/Controller/Main.pm
@@ -4,10 +4,65 @@ use Mojo::Base 'Mojolicious::Controller';
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
 
-# This action will render a template
 sub index ($self) {
-  # Render template "example/welcome.html.ep" with message
-  $self->render(msg => 'The main index page');
-}
+	$self->render( title => 'Use.perl.org archive');
+	}
+
+# about
+sub about ($self) {
+	$self->render;
+	}
+
+# stories?page=\d+
+sub stories ($self) {
+	my $page = $self->page;
+
+	$self->render;
+	}
+
+# authors?page=\d+
+sub authors ($self) {
+	my $page = $self->page;
+
+	$self->render;
+	}
+
+# journals?page=\d+
+sub journals ($self) {
+	my $page = $self->page;
+
+	$self->render;
+	}
+
+# article.pl?sid=\d+
+sub story ($self) {
+	my( $sid ) = $self->req->query_params->param( 'sid' );
+
+	$self->render;
+	}
+
+# ~author_name
+# ~author_name/
+sub author ($self) {
+	my( $author ) = $self->param( 'author' );
+
+	$self->render( author => $author );
+	}
+
+# ~author_name/journal/\d+
+sub journal ($self) {
+	my( $author ) = $self->param( 'author' );
+	my( $sid )    = $self->param( 'journal' );
+
+	$self->render(
+		sid    => $sid,
+		author => $author
+		);
+	}
+
+
+sub page ($self) {
+	$self->req->query_params->param( 'page' ) // 1;
+	}
 
 1;
